b406269dbc147753391b23e1ed8dba26718e1b39
Philippe Berglund <philippe05@gmail.com>
2016-10-04 20:34:27 +0200

    ready 95%

--- AdressBook/Form1.Designer.cs
+++ AdressBook/Form1.Designer.cs
@@ -28,12 +28,328 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxFirst = new System.Windows.Forms.TextBox();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.textBoxZip = new System.Windows.Forms.TextBox();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.textBoxPhone = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxSecond = new System.Windows.Forms.TextBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxOut = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.listBoxSearch = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Second Name";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Email @";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Address ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Zip Code";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "City";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 20);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "First Name";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 172);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 20);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Phone Number";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // textBoxFirst
+            // 
+            this.textBoxFirst.Location = new System.Drawing.Point(150, 49);
+            this.textBoxFirst.Name = "textBoxFirst";
+            this.textBoxFirst.Size = new System.Drawing.Size(171, 26);
+            this.textBoxFirst.TabIndex = 1;
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Location = new System.Drawing.Point(150, 295);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(171, 26);
+            this.textBoxCity.TabIndex = 7;
+            // 
+            // textBoxZip
+            // 
+            this.textBoxZip.Location = new System.Drawing.Point(150, 254);
+            this.textBoxZip.Name = "textBoxZip";
+            this.textBoxZip.Size = new System.Drawing.Size(171, 26);
+            this.textBoxZip.TabIndex = 6;
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Location = new System.Drawing.Point(150, 213);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(171, 26);
+            this.textBoxAdress.TabIndex = 5;
+            // 
+            // textBoxPhone
+            // 
+            this.textBoxPhone.Location = new System.Drawing.Point(150, 172);
+            this.textBoxPhone.Name = "textBoxPhone";
+            this.textBoxPhone.Size = new System.Drawing.Size(171, 26);
+            this.textBoxPhone.TabIndex = 4;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(150, 131);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(171, 26);
+            this.textBoxEmail.TabIndex = 3;
+            // 
+            // textBoxSecond
+            // 
+            this.textBoxSecond.Location = new System.Drawing.Point(150, 90);
+            this.textBoxSecond.Name = "textBoxSecond";
+            this.textBoxSecond.Size = new System.Drawing.Size(171, 26);
+            this.textBoxSecond.TabIndex = 2;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(673, 369);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(133, 42);
+            this.buttonRemove.TabIndex = 5;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(512, 369);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(133, 42);
+            this.buttonUpdate.TabIndex = 4;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(211, 369);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(149, 42);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save ";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxSecond);
+            this.groupBox1.Controls.Add(this.textBoxEmail);
+            this.groupBox1.Controls.Add(this.textBoxPhone);
+            this.groupBox1.Controls.Add(this.textBoxAdress);
+            this.groupBox1.Controls.Add(this.textBoxZip);
+            this.groupBox1.Controls.Add(this.textBoxCity);
+            this.groupBox1.Controls.Add(this.textBoxFirst);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(17, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(343, 342);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add New Person";
+            // 
+            // textBoxOut
+            // 
+            this.textBoxOut.Location = new System.Drawing.Point(17, 418);
+            this.textBoxOut.Multiline = true;
+            this.textBoxOut.Name = "textBoxOut";
+            this.textBoxOut.Size = new System.Drawing.Size(170, 87);
+            this.textBoxOut.TabIndex = 5;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(1012, 77);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 35);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(937, 92);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 20);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Result";
+            // 
+            // listBoxSearch
+            // 
+            this.listBoxSearch.FormattingEnabled = true;
+            this.listBoxSearch.ItemHeight = 20;
+            this.listBoxSearch.Location = new System.Drawing.Point(0, 136);
+            this.listBoxSearch.Name = "listBoxSearch";
+            this.listBoxSearch.Size = new System.Drawing.Size(666, 144);
+            this.listBoxSearch.TabIndex = 6;
+            this.listBoxSearch.SelectedIndexChanged += new System.EventHandler(this.listBoxSearch_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.listBoxSearch);
+            this.groupBox2.Location = new System.Drawing.Point(415, 10);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(706, 342);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search Index";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Last Name",
+            "Address",
+            "City"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(290, 28);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(797, 35);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(290, 26);
+            this.textBoxSearch.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1436, 526);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.textBoxOut);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Adress Book";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxFirst;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.TextBox textBoxZip;
+        private System.Windows.Forms.TextBox textBoxAdress;
+        private System.Windows.Forms.TextBox textBoxPhone;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxSecond;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxOut;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ListBox listBoxSearch;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxSearch;
     }
 }
 
--- AdressBook/Form1.cs
+++ AdressBook/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +18,268 @@ namespace AdressBook
         {
             InitializeComponent();
         }
+
+        // List<Person> personFile = new List<Person>();
+
+        public List<string> UpdateList = new List<string>();
+
+      
+        public void buttonSave_Click(object sender, EventArgs e)
+        {
+           // Person person = new Person
+        
+                var FirstName = textBoxFirst.Text;
+                var SecondName = textBoxSecond.Text;
+                var Email = textBoxEmail.Text;
+                var Phone = textBoxPhone.Text;
+                var Address = textBoxAdress.Text;
+                var Zip = textBoxZip.Text;
+                var City = textBoxCity.Text;
+            
+
+            textBoxFirst.Clear();
+            textBoxSecond.Clear();
+            textBoxEmail.Clear();
+            textBoxPhone.Clear();
+            textBoxAdress.Clear();
+            textBoxZip.Clear();
+            textBoxCity.Clear();
+
+
+            //person.Save(person.FirstName, person.SecondName, person.Email, person.Phone, person.Address, person.Zip,
+            //    person.City);
+
+
+            string toSave = FirstName + "," + SecondName + "," + Email + "," + Phone + "," +
+                            Address + "," + Zip + "," + City;
+
+            using (
+                StreamWriter path =
+                    new StreamWriter(@"C:\Users\phili\Desktop\Prog16\AdressBook\AdressBook\AddressBookLogg.txt", true))
+            {
+                path.WriteLine(toSave);
+            }
+
+            MessageBox.Show("Save Successful!");
+
+        }
+
+
+
+        List<string> LoadFile()
+        {
+            List<string> personFile = new List<string>();
+            string path = "C:\\Users\\phili\\Desktop\\Prog16\\AdressBook\\AdressBook\\AddressBookLogg.txt";
+            StreamReader sr = new StreamReader(path);
+            string pline;
+            while ((
+                pline
+                    =
+                    sr.ReadLine
+                        ()) != null)
+            {
+                personFile.Add(pline);
+            }
+            sr.Close
+                ();
+            return
+                personFile;
+
+        }
+
+
+
+        public List<string> GetNamesFromFile(string combo, string search)
+
+        {
+            int index = 0;
+
+            if (combo == "Last Name")
+            {
+                index = 1;
+            }
+
+            if (combo == "Address")
+            {
+                index = 3;
+            }
+            if (combo == "City")
+            {
+                index = 5;
+            }
+
+            List<string> contacts = new List<string>();
+            string path = "C:\\Users\\phili\\Desktop\\Prog16\\AdressBook\\AdressBook\\AddressBookLogg.txt";
+            StreamReader sr = new StreamReader(path);
+            string pline;
+            while ((pline = sr.ReadLine()) != null)
+            {
+                string[] array = pline.Split(',');
+
+                if (array[index] == search)
+                {
+                    contacts.Add(pline);
+                }
+            }
+            sr.Close();
+            return contacts;
+
+        }
+
+
+        public void Search_Click(object sender, EventArgs e)
+        {
+
+
+            // listBoxSearch.Text = contact;
+            if (comboBox1.SelectedItem != null && !string.IsNullOrEmpty(textBoxSearch.Text))
+
+            {
+                List<string> searchContact = GetNamesFromFile(comboBox1.SelectedItem.ToString(),
+                    textBoxSearch.Text.Trim());
+
+                listBoxSearch.DataSource = searchContact;
+            }
+
+
+            // vet ej om behövs -----------------------------------
+            //    string[] lines =
+            //    File.ReadAllLines(@"C:\Users\phili\Desktop\Prog16\AdressBook\AdressBook\AddressBookLogg.txt");
+
+            //foreach (string output in lines)
+            //{
+
+            //    if ()
+            //    {
+
+
+            //    }
+
+
+            //    List<string> plist = LoadFile();
+
+            //    listBoxSearch.DataSource = plist;
+
+
+
+        }
+
+        private void listBoxSearch_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] outList = listBoxSearch.SelectedItem.ToString().Split(',');
+
+            UpdateList = outList.ToList();
+
+            textBoxFirst.Text = outList[0];
+            textBoxSecond.Text = outList[1];
+            textBoxEmail.Text = outList[2];
+            textBoxPhone.Text = outList[3];
+            textBoxAdress.Text = outList[4];
+            textBoxZip.Text = outList[5];
+            textBoxCity.Text = outList[6];
+
+        }
+
+
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            List<string> newSaveList = new List<string>();
+
+            if (listBoxSearch.SelectedItem != null)
+            {
+                string listString = listBoxSearch.SelectedItem.ToString();
+
+                List<string> backList = LoadFile();
+                foreach (var line in backList)
+                {
+                    if (line != listString)
+                    {
+                        newSaveList.Add(line);
+                    }
+                }
+
+                File.WriteAllLines(@"C:\Users\phili\Desktop\Prog16\AdressBook\AdressBook\AddressBookLogg.txt",
+                    newSaveList.ToArray());
+
+                var FirstName = textBoxFirst.Text;
+                var SecondName = textBoxSecond.Text;
+                var Email = textBoxEmail.Text;
+                var Phone = textBoxPhone.Text;
+                var Address = textBoxAdress.Text;
+                var Zip = textBoxZip.Text;
+                var City = textBoxCity.Text;
+
+
+
+                string toSave = FirstName + "," + SecondName + "," + Email + "," + Phone + "," +
+                                Address + "," + Zip + "," + City;
+
+                using (
+                    StreamWriter path =
+                        new StreamWriter(@"C:\Users\phili\Desktop\Prog16\AdressBook\AdressBook\AddressBookLogg.txt",
+                            true))
+                {
+                    path.WriteLine(toSave);
+                }
+
+
+                MessageBox.Show("Update Successful!");
+
+            }
+
+
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            List<string> newSaveList = new List<string>();
+
+            if (listBoxSearch.SelectedItem != null)
+            {
+                string listString = listBoxSearch.SelectedItem.ToString();
+
+                List<string> backList = LoadFile();
+                foreach (var line in backList)
+                {
+                    if (line != listString)
+                    {
+                        newSaveList.Add(line);
+                    }
+                }
+
+                File.WriteAllLines(@"C:\Users\phili\Desktop\Prog16\AdressBook\AdressBook\AddressBookLogg.txt",
+                    newSaveList.ToArray());
+
+                textBoxFirst.Clear();
+                textBoxSecond.Clear();
+                textBoxEmail.Clear();
+                textBoxPhone.Clear();
+                textBoxAdress.Clear();
+                textBoxZip.Clear();
+                textBoxCity.Clear();
+
+
+                MessageBox.Show("Remove Successful!");
+
+
+            }
+
+
+
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+    
